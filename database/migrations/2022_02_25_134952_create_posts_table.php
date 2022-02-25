659f62d667acda6d5cefa7eb1df7fb7a529b6c43
prolaxu <adhikarilaxu0001@gmail.com>
2022-02-25 19:40:09 +0545

    Enhc: posts migration and model.

---
+++ database/migrations/2022_02_25_134952_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->text('feature_image');
+            $table->string('seo_title');
+            $table->text('seo_description');
+            $table->text('seo_keywords');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
